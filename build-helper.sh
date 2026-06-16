4b78cc798cb89e95b551beca15fbf0cc285004de
Toporin <satochip.wallet@gmail.com>
2026-06-16 14:18:56 +0200

    Use seedkeeper-manager instead of yubikey-manager package from PyPI

--- build-helper.sh
+++ build-helper.sh
@@ -79,8 +79,8 @@ VENV="../$OUTPUT/helper-license-venv"
 rm -rf $VENV
 uv run python -m venv $VENV
 $VENV/bin/pip install --upgrade pip wheel
+#$VENV/bin/pip install --force-reinstall ../../seedkeeper-manager # DEBUG only for local dev package
 $VENV/bin/pip install dist/authenticator_helper-0.1.0-py3-none-any.whl pip-licenses
-$VENV/bin/pip install --force-reinstall ../../yubikey-manager
 $VENV/bin/pip-licenses --format=json --no-license-path --with-license-file --ignore-packages authenticator-helper zxing-cpp --output-file ../assets/licenses/helper.json
 cd ..
 
