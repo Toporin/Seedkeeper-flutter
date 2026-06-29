ebfd3b4e053b0e5174adb55ff6db06e3f70899de
Toporin <satochip.wallet@gmail.com>
2026-06-29 16:47:57 +0200

    UI: Change color theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -18,7 +18,9 @@ import 'package:flutter/material.dart';
 
 import 'core/state.dart';
 
-const defaultPrimaryColor = Colors.lightGreen;
+// const defaultPrimaryColor = Colors.lightGreen;
+// const defaultPrimaryColor = Color(0xFFB09162);
+const defaultPrimaryColor = Color(0xFF9c7af2);
 
 class AppTheme {
   static ThemeData getLightTheme(Color primaryColor) =>
